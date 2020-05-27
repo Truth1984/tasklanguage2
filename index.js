6ff7deeb08cd5e8897f831026e03be02f4c0021d
truth1984 <zhengyao365@163.com>
2020-05-27 10:21:04 +0800

    fix error

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ module.exports = class {
   async run(name) {
     let sorted = Array.from(this._localcmd).sort((a, b) => (a.name <= b.name ? -1 : 1));
     for (let i = 0; i < sorted.length - 1; i++)
-      if (sorted[i].name === sorted[i + 1].name) throw new Error("name duplicated: ", sorted[i].name);
+      if (sorted[i].name === sorted[i + 1].name) throw new Error("name duplicated: " + sorted[i].name);
 
     let startIndex = name === undefined ? 0 : this._localcmd.findIndex((item) => item.name === name);
-    if (startIndex === -1) throw new Error("start name not found ", name);
+    if (startIndex === -1) throw new Error("start name not found: " + name);
 
     for (let i = startIndex; i < this._localcmd.length; ) {
       let next = (names) => (i = names === undefined ? i + 1 : this._localcmd.findIndex((item) => item.name === names));
